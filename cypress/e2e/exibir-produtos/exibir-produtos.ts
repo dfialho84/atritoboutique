310edf3389e1e887b3abc9bb631332ac580e481c
Diego Fialho Rodrigues <diego.fialho@ufv.br>
2026-05-03 10:30:31 -0300

    clean

--- cypress/e2e/exibir-produtos/exibir-produtos.ts
+++
@@ -1,9 +0,0 @@
-import { Given, Then } from "@badeball/cypress-cucumber-preprocessor";
-
-Given("Eu visito a página {string}", (url: string) => {
-    cy.visit(url);
-});
-
-Then("Eu vejo alguns produtos", () => {
-    cy.contains("Nome do Produto").should("be.visible");
-});
